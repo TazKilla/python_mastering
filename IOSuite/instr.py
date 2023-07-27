72b1e71210b1fc1c4efde555054837cc707badbf
Guillaume Roche <groche@cochlear.com>
2023-07-27 11:04:15 +0200

    Add docstrings to project

--- IOSuite/instr.py
+++ IOSuite/instr.py
@@ -1,7 +1,13 @@
 import enum
+import pyvisa
+
+class RangeOptions(enum.Enum):
+    minRange = "MIN"
+    maxRange = "MAX"
+    defRange = "DEF"
 
 class OutputStates(enum.Enum):
-    """Device's output state. Can be on or off."""
+    """Device's output state."""
 
     on      = "on"
     off     = "off"
@@ -17,24 +23,42 @@ class Waveshapes(enum.Enum):
     dc      = "DC"
     user    = "USER"
 
-class StepCharac(enum.Enum):
+class ValueToMeas(enum.Enum):
     """Current characteristic for step setting."""
 
-    voltage = "VOLT"
-    current = "CURR"
+    voltage     = "VOLT"
+    current     = "CURR"
+    resistance  = "RES"     # Two-wire ohms measurement
+    fresistance = "FRES"    # Four-wire ohms measurement
+    frequency   = "FREQ"
+    period      = "PER"
+    continuity  = "CONT"
+    diode       = "DIOD"
 
 class CurrentType(enum.Enum):
     """Type of current to supply or to measure."""
-    
+
     alternating = "AC"
     direct      = "DC"
 
 class Instrument():
-    """Class meant to define a generic measurement instrument using GPIB
-        connection, and must be inherited by device's specific classes."""
+    """
+    Class meant to define a generic measurement instrument using GPIB
+    connection, and must be inherited by device's specific classes.
 
-    def __init__(self, resource):
-        self.resource = resource
+    Methods
+    -------
+    * __init__()        - Called when the class is instantiated.
+    * __str__()         - Called when trying to print a class instance.
+    * getID()           - Query the instrument's ID string.
+    * reset()           - Reset the instrument.
+    * clearStatus()     - Clear device's internal status.
+    * writeText()       - Write custom text on device's screen.
+    * getErrorQueue()   - Read and log device's error queue.
+    """
+
+    def __init__(self, rm: pyvisa.ResourceManager, resource: str):
+        self.resource = rm.open_resource(resource)
         self.description = "Generic instrument"
     
     def __str__(self):
@@ -49,22 +73,47 @@ class Instrument():
         """Reset the instrument."""
 
         print(self.resource.write("*RST"))
+    
+    def clearStatus(self):
+        """Clear device's internal status."""
 
+        print(self.resource.write("*CLS"))
+    
+    def writeText(self, text: str):
+        """Write custom text on device's screen."""
+
+        print(self.resource.write('DISP:TEXT "{0}"'.format(text)))
+    
+    def getErrorQueue(self):
+        """Read and log device's error queue."""
+
+        print("Error queue from " + self.description)
+        print(self.resource.query("SYST:ERR?"))
 
 class FrequencyGenerator_33220A(Instrument):
-    """Class meant to define the Agilent 33220A Waveform Generator."""
+    """
+    Class meant to define the Agilent 33220A Waveform Generator.
 
-    def __init__(self, resource):
-        Instrument.__init__(self, resource)
-        self.description = "20MHz Function/Arbitrary Waveform generator - 33220A"
+    Methods
+    -------
+    * setOutput()   - Set the generator's output state.
+    * setWaveform() - Set up the waveform to generate.
+    """
 
-    def setOutput(self, state):
+    def __init__(self, rm: pyvisa.ResourceManager, resource: str):
+        Instrument.__init__(self, rm, resource)
+        self.description = "20MHz Function/Arbitrary Waveform Generator - 33220A"
+
+    def setOutput(self, state: OutputStates):
         """Set the generator's output state. It can be on or off."""
 
-        print("state:", state)
-        print(self.resource.write("OUTP {0}".format(state)))
+        print("state:", state.value)
+        print(self.resource.write("OUTP {0}".format(state.value)))
     
-    def setWaveform(self, form, load=50, lowVol=0, highVol=0.75, period=1e-3, width=100e-6, trans=10e-9, freq=2500, ampl=1.2, offset=0.4):
+    def setWaveform(self, form: Waveshapes, load: int=50, lowVol: float=0,
+                    highVol: float=0.75, period: float=1e-3, width:float=100e-6,
+                    trans: float=10e-9, freq:float=2500, ampl: float=1.2,
+                    offset: float=0.4):
         """
         Set up the waveform to generate.
 
@@ -84,7 +133,7 @@ class FrequencyGenerator_33220A(Instrument):
             Pulse width in seconds (default is 100 µs)
         trans : float, optional
             Edge time, where rise time = fall time in seconds (default is 10 ns)
-        freq : int, optional
+        freq : float, optional
             Signal frequency (default is 2500 Hz)
         ampl : float, optional
             Signal amplitude in Vpp (default is 1.2 Vpp)
@@ -113,65 +162,100 @@ class FrequencyGenerator_33220A(Instrument):
         None
         """
         
-        self.resource.write("FUNC {0}".format(form))
+        self.resource.write("FUNC {0}".format(form.value))
         self.resource.write("OUTP:LOAD {0}".format(load))
-        if form == Waveshapes.pulse.value:
+        if form == Waveshapes.pulse:
             self.resource.write("VOLT:LOW {0}".format(lowVol))
             self.resource.write("VOLT:HIGH {0}".format(highVol))
             self.resource.write("PULS:PER {0}".format(period))
             self.resource.write("PULS:WIDT {0}".format(width))
             self.resource.write("PULS:TRAN {0}".format(trans))
-        elif form == Waveshapes.sine.value:
+        elif form == Waveshapes.sine:
             self.resource.write("FREQ {0}".format(freq))
             self.resource.write("VOLT {0}".format(ampl))
             self.resource.write("VOLT:OFFS {0}".format(offset))
         else:
-            print("Waveform not implemented.")
+            print("No implementation for {0} waveform.".format(form.value))
 
 
 class MultiMeter_34401A(Instrument):
-    """Class meant to define the Agilent 34401A Multimeter"""
+    """
+    Class meant to define the Agilent 34401A Multimeter
+    
+    Methods
+    -------
+    * setMeasurements() - Set the way to run measurements.
+    * getMeas()         - Get value measurement from current measurement setup.
+    * read()            - Simply read the value shown on the device.
+    """
 
-    def __init__(self, resource):
-        Instrument.__init__(self, resource)
+    def __init__(self, rm: pyvisa.ResourceManager, resource: str):
+        Instrument.__init__(self, rm, resource)
         self.description = "Agilent 6 1/2 Digit Multimeter - 34401A"
     
-    def setMeasurements(self, type=CurrentType.direct.value, voltRange=10, resolution=0.001):
-        print(self.resource.write("CONF:VOLT:{0} {1}, {2}".format(type, voltRange, resolution)))
+    def setMeasurements(self, type: CurrentType=CurrentType.direct,
+                        voltRange: float=10, resolution: float=0.001):
+        """Set the way to run measurements. No measure is made at this step."""
 
-    def getMeasVolt(self):
-        print(self.resource.query_ascii_values("MEAS:VOLT:DC?"))
+        # print(self.resource.write("CONF:CURR:{0} {1}, {2}".format(type.value, voltRange, resolution)))
+        print(self.resource.write("CONF:CURR:DC MAX, MAX"))
+
+    def getMeas(self, mode: ValueToMeas, curr_type: CurrentType):
+        """Get value measurement from current measurement setup."""
+
+        print(self.resource.query_ascii_values("MEAS:{0}:{1}?".format(mode.value, curr_type.value)))
     
     def read(self):
+        """Simply read the value shown on the device."""
+
         return self.resource.query("READ?")
 
 
 class PowerSupply_E3644A(Instrument):
-    """Class meant to define the Agilent E3644A Power Supply"""
+    """
+    Class meant to define the Agilent E3644A Power Supply.
 
-    def __init__(self, resource):
-        Instrument.__init__(self, resource)
+    Methods
+    -------
+    * setPower()    - Set the power generated by the power supply device.
+    * setOutput()   - Set the generator's output state. It can be on or off.
+    * setStepSize() - Set the step size used when calling voltUp() or voltDown().
+    * voltUp()      - Increase the volt value provided by the device.
+    * voltDown()    - Decrease the volt value provided by the device.
+    * getMeasCurr() - Return the current provided by the device, in Amperes.
+    """
+
+    def __init__(self, rm: pyvisa.ResourceManager, resource: str):
+        Instrument.__init__(self, rm, resource)
         self.description = "Agilent DC Power Supply - E3644A\n \
                             0-8V, 8A / 0-20V, 4A"
     
-    def setPower(self, volt, maxAmp):
+    def setPower(self, volt: float, maxAmp: float):
+        """Set the power generated by the power supply device."""
+
         print(self.resource.write("APPL {0}, {1}".format(volt, maxAmp)))
 
-    def setOutput(self, state):
-        print("state:", state)
-        print(self.resource.write("OUTP {0}".format(state)))
+    def setOutput(self, state: OutputStates):
+        """Set the generator's output state."""
+
+        print("state:", state.value)
+        print(self.resource.write("OUTP {0}".format(state.value)))
     
-    def setStepSize(self, mode, step):
-        print(self.resource.write("{0}:STEP {1}".format(mode, step)))
+    def setStepSize(self, mode: ValueToMeas, step: float):
+        """Set the step size used when calling voltUp() or voltDown()."""
+
+        print(self.resource.write("{0}:STEP {1}".format(mode.value, step)))
 
     def voltUp(self):
+        """Increase the volt value provided by the device, based on the selected
+        step size"""
         print(self.resource.write("VOLT UP"))
 
     def voltDown(self):
+        """Decrease the volt value provided by the device, based on the selected
+        step size"""
         print(self.resource.write("VOLT DOWN"))
-    
-    def clearStatus(self):
-        print(self.resource.write("*CLS"))
 
     def getMeasCurr(self):
+        """Return the current provided by the device, in Amperes."""
         print(self.resource.query("MEAS:CURR?"))
--- IOSuite/test_tools.py
+++ IOSuite/test_tools.py
@@ -17,17 +17,17 @@ def printData(width, data, date, sampleRate, sine_wave, sine_ampl):
               str(sampleRate) + ' Sa/s, $f$ = ' + str(sine_wave) + 'Hz, $v_0$ = ' + str(sine_ampl) + 'V$_{pp}$')
     plt.grid(True)
     plt.savefig('{0}_measurement_result.png'.format(date))
-    plt.show()
+    # plt.show()
 
-sine_wave       = 1
-sine_ampl       = 0.5
+sineWave       = 1
+sineAmpl       = 0.5
 rm              = pyvisa.ResourceManager()
 res_list        = rm.list_resources()
 print(res_list)
 
-# power_supply    = PowerSupply_E3644A(rm.open_resource(res_list[0]))
-frequ_gen       = FrequencyGenerator_33220A(rm.open_resource(res_list[1]))
-multimeter      = MultiMeter_34401A(rm.open_resource(res_list[2]))
+# power_supply    = PowerSupply_E3644A(rm, res_list[0])
+frequ_gen       = FrequencyGenerator_33220A(rm, res_list[1])
+multimeter      = MultiMeter_34401A(rm, res_list[2])
 
 # print(power_supply)
 print(frequ_gen)
@@ -35,21 +35,22 @@ print(multimeter)
 
 frequ_gen.getID()
 frequ_gen.reset()
-# frequ_gen.setWaveform(Waveshapes.pulse.value, load=50, highVol=0, lowVol=0.75, period=1e-3, width=100e-6, trans=10e-9)
-frequ_gen.setWaveform(Waveshapes.sine.value, load=50, freq=sine_wave, ampl=sine_ampl, offset=0)
-frequ_gen.setOutput(OutputStates.on.value)
+frequ_gen.setWaveform(Waveshapes.pulse, load=50, lowVol=0, highVol=2, period=0.5, width=0.2, trans=5e-9)
+# frequ_gen.setWaveform(Waveshapes.sine, load=50, freq=sine_wave, ampl=sine_ampl, offset=0)
+frequ_gen.setOutput(OutputStates.on)
 
 multimeter.getID()
 multimeter.reset()
-multimeter.setMeasurements()
+multimeter.setMeasurements(type=CurrentType.alternating)
 
 # power_supply.getID()
 # power_supply.reset()
-# power_supply.setStepSize(StepMode.voltage.value, 0.2)
+# power_supply.setStepSize(StepMode.voltage, 0.2)
 # power_supply.setPower(3.2, 0.25)
-# power_supply.setOutput(OutputStates.on.value)
+# power_supply.setOutput(OutputStates.on)
 
 N = 2048
+# N = 200
 # N = 512
 data = pd.DataFrame(columns=['t', 'V'])
 date = datetime.datetime.now().strftime('%Y.%m.%d_%Hh%Mm%Ss%f')
@@ -57,18 +58,28 @@ date = datetime.datetime.now().strftime('%Y.%m.%d_%Hh%Mm%Ss%f')
 print(date)
 t0 = time.time()
 for i in range(N):
+    if i % 20 == 0:
+        print("Iteration {0}".format(i))
     data.loc[i] = [time.time() - t0, multimeter.read()]
 
 data.V = data.V.astype(float)
 
 # print(data)
-elapseTime = data.iloc[N-1].t
-sampleRate = str(np.round((len(data)-1)/elapseTime, 3))
-print("Elapsed time: ", elapseTime)
+elapsedTime = data.iloc[N-1].t
+sampleRate = str(np.round((len(data)-1)/elapsedTime, 3))
+print("Elapsed time: ", elapsedTime)
 print("Sampling rate: ", sampleRate)
 
-printData(N, data, date, sampleRate, sine_wave, sine_ampl)
+printData(N, data, date, sampleRate, sineWave, sineAmpl)
 
-multimeter.resource.write('DISP:TEXT "JOB DONE"')
+frequ_gen.writeText("Cleaning buffer...")
+frequ_gen.getErrorQueue()
+frequ_gen.clearStatus()
+multimeter.writeText("Cleaning buffer...")
+multimeter.getErrorQueue()
+multimeter.clearStatus()
+
+frequ_gen.writeText("JOB DONE")
+multimeter.writeText("JOB DONE")
 
 # power_supply.setOutput(instr.OutputStates.off.value)
